7769873cc75410e5d4d07315ddc9c5f58a7dd24a
ToksichniyDed <dimadyakonov9620@yandex.ru>
2024-05-07 03:13:59 +0300

    Зафиксированные изменения перед переписью истории коммитов

---
+++ Server/Tests/cmake-build-debug/googletest-1.14.0/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Progg/Plotniy_Project/Server/Tests/googletest-1.14.0/googletest
+# Build directory: D:/Progg/Plotniy_Project/Server/Tests/cmake-build-debug/googletest-1.14.0/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
